f3ec95440768f592254a45d84fdd41b32f3b63ce
shved <shvedujk@gmail.com>
2020-09-13 22:09:11 +0300

    connect opencv, open image by arg value

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+#include <opencv2/opencv.hpp>
+
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    printf("It works.");
+    cv::Mat src;
+    if (argc != 2 || !(src = cv::imread(argv[1], 1)).data)
+        return -1;
+
+    printf("image size %d x %d\n", src.rows, src.cols);
     return 0;
 }
